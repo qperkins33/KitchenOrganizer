ece406d7bf1fbd52e33b31238b3250318224582e
qperkins33 <quin.perkins@my.metrostate.edu>
2024-02-15 22:30:46 -0600

    Merge remote-tracking branch 'origin/main-branch' into main-branch
    # Conflicts:
    #	src/main/java/org/example/kitchenorganizer/HelloApplication.java

--- src/main/java/org/example/kitchenorganizer/FoodCollection.java
+++ src/main/java/org/example/kitchenorganizer/FoodCollection.java
@@ -1,5 +1,27 @@
 package org.example.kitchenorganizer;
 
-public class FoodCollection {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+public class FoodCollection {
+    private List<Food> items;
+
+    public FoodCollection() {
+        this.items = new ArrayList<Food>();
+    }
+
+    public void sortByName() {
+        items.sort(Comparator.comparing(Food::getName));
+    }
+
+//    + SortByExpiration()
+//
+//    + AddItem(item: Food): void
+//
+//    + RemoveItem(item: Food): void
+//
+//    + GetItems(items: String):
+//
+//    + GetItemList: List<Food>
 }
