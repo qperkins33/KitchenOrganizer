a8c7f2889b7eafb2244af49a028f2a971b89f8e3
qperkins33 <quin.perkins@my.metrostate.edu>
2024-02-15 19:19:47 -0600

    Updated Food and InventoryItem

--- src/main/java/org/example/kitchenorganizer/Food.java
+++ src/main/java/org/example/kitchenorganizer/Food.java
@@ -1,5 +1,62 @@
 package org.example.kitchenorganizer;
 
-public class Food {
+public class Food extends InventoryItem {
 
+    private int expDate;
+
+    // Constructor
+    public Food(String name, double quantity, String measurementUnit, double minQuantity, int expDate) {
+        super(name, quantity, measurementUnit, minQuantity); // Call the superclass constructor
+        this.expDate = expDate;
+    }
+
+    // Methods
+    public void setExpDate(int expDate) {
+        this.expDate = expDate;
+    }
+
+    public int getExpDate() {
+        return this.expDate;
+    }
+
+    // Implementing abstract methods from InventoryItem
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public double getQuantity() {
+        return this.quantity;
+    }
+
+    @Override
+    public void setMeasurementUnit(String measurementUnit) {
+        this.measurementUnit = measurementUnit;
+    }
+
+    @Override
+    public String getMeasurementUnit() {
+        return this.measurementUnit;
+    }
+
+    @Override
+    public void setMinQuantity(double minQuantity) {
+        this.minQuantity = minQuantity;
+    }
+
+    @Override
+    public double getMinQuantity() {
+        return this.minQuantity;
+    }
 }
--- src/main/java/org/example/kitchenorganizer/InventoryItem.java
+++ src/main/java/org/example/kitchenorganizer/InventoryItem.java
@@ -1,5 +1,41 @@
 package org.example.kitchenorganizer;
 
-public class InventoryItem {
+public abstract class InventoryItem {
 
+    // Attributes common to all inventory items
+    protected String name;
+    protected double quantity;
+    protected String measurementUnit;
+    protected double minQuantity;
+
+    // Constructor
+    public InventoryItem(String name, double quantity, String measurementUnit, double minQuantity) {
+        this.name = name;
+        this.quantity = quantity;
+        this.measurementUnit = measurementUnit;
+        this.minQuantity = minQuantity;
+    }
+
+    // Abstract methods - to be implemented by subclasses
+    public abstract void setName(String name);
+    public abstract void setQuantity(double quantity);
+    public abstract String getName();
+    public abstract double getQuantity();
+
+    // Concrete methods - common implementation across all subclasses
+    public String getMeasurementUnit() {
+        return measurementUnit;
+    }
+
+    public void setMeasurementUnit(String measurementUnit) {
+        this.measurementUnit = measurementUnit;
+    }
+
+    public double getMinQuantity() {
+        return minQuantity;
+    }
+
+    public void setMinQuantity(double minQuantity) {
+        this.minQuantity = minQuantity;
+    }
 }
