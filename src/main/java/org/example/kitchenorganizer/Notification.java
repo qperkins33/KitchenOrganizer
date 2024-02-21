3c3d37e5b13246ffdb4c19c8ac8f4b9d0edce174
qperkins33 <quin.perkins@my.metrostate.edu>
2024-02-21 13:29:57 -0600

    Created Notification class

---
+++ src/main/java/org/example/kitchenorganizer/Notification.java
@@ -0,0 +1,14 @@
+package org.example.kitchenorganizer;
+
+public class Notification {
+    private User currentUser;
+    private String lowQuantityMessage = "Low Quantity Message";
+
+//    +Notification(currentUser: User)
+//
+//    +getCurrentUser(): User
+//
+//    +getLowQuantityMessage: String
+//
+//    +checkUserInventoryQuantity(): Food
+}
