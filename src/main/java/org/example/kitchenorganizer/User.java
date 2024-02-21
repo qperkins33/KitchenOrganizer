7e86e7b4f26f9190a67384523b6addfca1cfc3c3
qperkins33 <quin.perkins@my.metrostate.edu>
2024-02-21 13:26:52 -0600

    Updated User class

--- src/main/java/org/example/kitchenorganizer/User.java
+++ src/main/java/org/example/kitchenorganizer/User.java
@@ -1,4 +1,32 @@
 package org.example.kitchenorganizer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String name;
+    private List<FoodCollection> foodInventoryList;
+
+    // Constructor
+    public User(String name) {
+        this.name = name;
+        this.foodInventoryList = new ArrayList<>();
+    }
+
+    // Methods
+    public String getName() {
+        return name;
+    }
+
+    public List<FoodCollection> getFoodInventoryList() {
+        return foodInventoryList;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addCollectionToInventoryList(List<FoodCollection> newCollection) {
+        this.foodInventoryList = newCollection;
+    }
 }
